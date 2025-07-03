f8d6290f76ca3e902b988a60c6580dd871f33afe
Nuno David <ndavidq0@gmail.com>
2025-07-03 12:18:58 +0100

    feat: add cw4626 package; add instantiate and query msgs

---
+++ packages/cw4626/README.md
@@ -0,0 +1,4 @@
+# CW4626 Spec Tokenized Vaults
+
+Based on [ERC-4626](https://eips.ethereum.org/EIPS/eip-4626) and its
+[OpenZeppelin implementation](https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC4626.sol).
---
+++ packages/cw4626/src/msg.rs
@@ -0,0 +1,140 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{Addr, Uint128};
+
+#[cw_serde]
+pub struct Cw4626InstantiateMsg {
+    pub owner: Option<Addr>,
+    pub share_token_address: Addr,
+    pub underlying_token_address: Addr,
+}
+
+#[cw_serde]
+pub enum Cw4626ExecuteMsg {}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum Cw4626QueryMsg {
+    /// Since the share token is separated from the vault, added a getter to the share
+    /// Returns the address of the share cw20 token used for the Vault for accounting, depositing, and withdrawing
+    #[returns(ShareResponse)]
+    Share {},
+    /// Returns the address of the underlying cw20 token used for the Vault for accounting, depositing, and withdrawing
+    #[returns(AssetResponse)]
+    Asset {},
+    /// Since the share token is separated from the vault, added a getter to the total shares
+    /// Returns the total amount of the share asset that is managed by Vault
+    #[returns(TotalSharesResponse)]
+    TotalShares {},
+    /// Returns the total amount of the underlying asset that is managed by Vault
+    #[returns(TotalAssetsResponse)]
+    TotalAssets {},
+    /// Returns the amount of shares that the Vault would exchange for the amount of assets provided, in an ideal
+    /// scenario where all the conditions are met
+    #[returns(ConvertToSharesResponse)]
+    ConvertToShares { assets: Uint128 },
+    /// Returns the amount of assets that the Vault would exchange for the amount of shares provided, in an ideal
+    /// scenario where all the conditions are met
+    #[returns(ConvertToAssetsResponse)]
+    ConvertToAssets { shares: Uint128 },
+    /// Returns the maximum amount of the underlying asset that can be deposited into the Vault for the receiver,
+    /// through a deposit call
+    #[returns(MaxDepositResponse)]
+    MaxDeposit { receiver: Addr },
+    /// Allows an on-chain or off-chain user to simulate the effects of their deposit at the current block, given
+    /// current on-chain conditions
+    #[returns(PreviewDepositResponse)]
+    PreviewDeposit { assets: Uint128 },
+    /// Returns the maximum amount of the Vault shares that can be minted for the receiver, through a mint call
+    #[returns(MaxMintResponse)]
+    MaxMint { receiver: Addr },
+    /// Allows an on-chain or off-chain user to simulate the effects of their mint at the current block, given
+    /// current on-chain conditions
+    #[returns(PreviewMintResponse)]
+    PreviewMint { shares: Uint128 },
+    /// Returns the maximum amount of the underlying asset that can be withdrawn from the owner balance in the
+    /// Vault, through a withdraw call
+    #[returns(MaxWithdrawResponse)]
+    MaxWithdraw { owner: Addr },
+    /// Allows an on-chain or off-chain user to simulate the effects of their withdrawal at the current block,
+    /// given current on-chain conditions
+    #[returns(PreviewWithdrawResponse)]
+    PreviewWithdraw { assets: Uint128 },
+    /// Returns the maximum amount of Vault shares that can be redeemed from the owner balance in the Vault,
+    /// through a redeem call
+    #[returns(MaxRedeemResponse)]
+    MaxRedeem { owner: Addr },
+    /// Allows an on-chain or off-chain user to simulate the effects of their redemption at the current block,
+    /// given current on-chain conditions
+    #[returns(PreviewRedeemResponse)]
+    PreviewRedeem { shares: Uint128 },
+}
+
+#[cw_serde]
+pub struct ShareResponse {
+    pub share_token_address: Addr,
+}
+
+#[cw_serde]
+pub struct AssetResponse {
+    pub asset_token_address: Addr,
+}
+
+#[cw_serde]
+pub struct TotalSharesResponse {
+    pub total_managed_shares: Uint128,
+}
+
+#[cw_serde]
+pub struct TotalAssetsResponse {
+    pub total_managed_assets: Uint128,
+}
+
+#[cw_serde]
+pub struct ConvertToSharesResponse {
+    pub shares: Uint128,
+}
+
+#[cw_serde]
+pub struct ConvertToAssetsResponse {
+    pub assets: Uint128,
+}
+
+#[cw_serde]
+pub struct MaxDepositResponse {
+    pub max_assets: Uint128,
+}
+
+#[cw_serde]
+pub struct PreviewDepositResponse {
+    pub shares: Uint128,
+}
+
+#[cw_serde]
+pub struct MaxMintResponse {
+    pub max_shares: Uint128,
+}
+
+#[cw_serde]
+pub struct PreviewMintResponse {
+    pub assets: Uint128,
+}
+
+#[cw_serde]
+pub struct MaxWithdrawResponse {
+    pub max_assets: Uint128,
+}
+
+#[cw_serde]
+pub struct PreviewWithdrawResponse {
+    pub shares: Uint128,
+}
+
+#[cw_serde]
+pub struct MaxRedeemResponse {
+    max_shares: Uint128,
+}
+
+#[cw_serde]
+pub struct PreviewRedeemResponse {
+    assets: Uint128,
+}
