9b38bd53fe51a1352aee5646093781922cc137e9
Nuno David <ndavidq0@gmail.com>
2025-07-04 19:27:16 +0100

    feat: implement allowance queries

--- contracts/cw4626-base/src/state.rs
+++ contracts/cw4626-base/src/state.rs
@@ -1,5 +1,9 @@
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw4626::WithdrawalShareAllowanceResponse;
+use cw_storage_plus::{Item, Map};
 
 pub const ASSET: Item<Addr> = Item::new("asset");
 pub const SHARE: Item<Addr> = Item::new("share");
+
+pub const WITHDRAWAL_SHARE_ALLOWANCES: Map<(&Addr, &Addr), WithdrawalShareAllowanceResponse> =
+    Map::new("allowance");
