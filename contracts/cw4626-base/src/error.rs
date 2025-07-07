1c6b3639f8476d6474d2604301164af132a939af
Nuno David <ndavidq0@gmail.com>
2025-07-07 21:32:29 +0100

    feat: add cw20 contract error to error mod

--- contracts/cw4626-base/src/error.rs
+++ contracts/cw4626-base/src/error.rs
@@ -1,4 +1,5 @@
 use cosmwasm_std::StdError;
+use cw20_base::ContractError as Cw20ContractError;
 use cw_ownable::OwnershipError;
 use thiserror::Error;
 
@@ -9,6 +10,9 @@ pub enum ContractError {
 
     #[error("{0}")]
     Ownership(#[from] OwnershipError),
+
+    #[error("{0}")]
+    ShareCw20Error(#[from] Cw20ContractError),
 
     #[error("{addr} is not a cw20")]
     InvalidCw20 { addr: String },
