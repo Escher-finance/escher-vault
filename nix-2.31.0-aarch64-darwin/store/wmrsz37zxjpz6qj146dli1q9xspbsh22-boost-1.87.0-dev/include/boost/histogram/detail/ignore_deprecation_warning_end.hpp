90ab2349d0044e3d57505ebc0a43e4bea910d308
ohadbachner <81328098+ohadbachner@users.noreply.github.com>
2025-09-01 12:55:13 +0300

    Fix .gitignore: Add Nix build artifacts and prevent 3M+ files from being tracked

--- nix-2.31.0-aarch64-darwin/store/wmrsz37zxjpz6qj146dli1q9xspbsh22-boost-1.87.0-dev/include/boost/histogram/detail/ignore_deprecation_warning_end.hpp
+++
@@ -1,7 +0,0 @@
-#line 1 "include/boost/histogram/detail/ignore_deprecation_warning_end.hpp"
-#if (defined(__GNUC__) || defined(__clang__))
-#pragma GCC diagnostic pop
-
-#elif defined(_MSC_VER)
-#pragma warning(pop)
-#endif
