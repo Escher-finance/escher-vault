d08921a83ded95673bd6daf020ce8fe769fdbd93
Nuno David <ndavidq0@gmail.com>
2025-09-09 11:03:05 +0100

    chore: remove the files

--- contracts/cw4626-base/src/msg.rs
+++
@@ -1,3 +0,0 @@
-pub use cw4626::Cw4626ExecuteMsg as ExecuteMsg;
-pub use cw4626::Cw4626InstantiateMsg as InstantiateMsg;
-pub use cw4626::Cw4626QueryMsg as QueryMsg;
--- packages/cw4626/src/lib.rs
+++
@@ -1,3 +0,0 @@
-mod msg;
-
-pub use msg::*;
